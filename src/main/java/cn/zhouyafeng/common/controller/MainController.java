660b96b997829964d465e90dfc0e9f0ee1bc8c95
yaphone <zhouyaphone@163.com>
2017-06-04 00:41:23 +0800

    sqlite支持

---
+++ src/main/java/cn/zhouyafeng/common/controller/MainController.java
@@ -0,0 +1,25 @@
+package cn.zhouyafeng.common.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 
+ * @author https://github.com/yaphone
+ * @date 创建时间：2017年6月4日 上午12:12:33
+ * @version 1.0
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/")
+public class MainController {
+	@RequestMapping("index")
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView("/index");
+		return modelAndView;
+	}
+
+}
